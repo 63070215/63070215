4e7124e9f94ea47890196447eeda9041eaaae779
63070215 <73693781+63070215@users.noreply.github.com>
2020-10-30 16:57:56 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <h1>My first Heading</h1>
+        <p>My first Paragraph</p>
+        <img src="Board_mite.jpg">
+        <br>
+        <a href="http://www.it.kmitl.ac.th">This is a Link</a>
+
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ul>
+    </body>
+</html>
