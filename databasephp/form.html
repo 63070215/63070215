5753a54208ebf2bc641a8b0799e8235ae86f453e
63070215 <73693781+63070215@users.noreply.github.com>
2020-11-13 17:20:25 +0700

    Add files via upload

---
+++ databasephp/form.html
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Comment Form</title>
+</head>
+<body>
+  <form action = "insert.php" method = "post" id="CommentForm" >
+    Name:<br>
+    <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
+    Comment:<br>
+    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"></textarea><br>  
+    Link:<br>
+    <input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
+    <input type="submit" id="commentBtn">
+  </form> 
+</body>
+</html>
---
+++ databasephp/insert.php
@@ -0,0 +1,26 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'labbatabase.mysql.database.azure.com', 'jirapun@labbatabase', 'eB8oytxbfd', 'ITFLab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$link = $_POST['link'];
+
+
+$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
