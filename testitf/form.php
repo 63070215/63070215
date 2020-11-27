ea6fa3112f2948953cf0dfdc6afc3b832c2a32b8
63070215 <73693781+63070215@users.noreply.github.com>
2020-11-27 17:02:10 +0700

    Add files via upload

---
+++ testitf/form.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Add</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+	<style type="text/css">
+		.card {
+			border-top: solid 5px #ffc107;
+		}
+	</style>
+</head>
+<body class="bg-primary py-5">
+	<div class="container text-black">
+		<div class="row">
+			<div class="col-12 col-lg-8 offset-lg-2">
+				<div class="card shadow">
+					<div class="card-body">
+						<div class="row">
+							<div class="col-6"><h1 class="text-monospace">กรอกข้อมูล</h1></div>
+						</div>
+						<form action="insert.php" method="post">
+							<div class="form-group">
+								<label>Name</label>
+								<input type="text" name="name" class="form-control" required>
+							</div>
+							<div class="form-group">
+								<label>Height</label>
+								<input type="float" step = "0.01" name="height" class="form-control" rows="5" required>
+							</div>
+							<div class="form-group">
+								<label>Weight</label>
+								<input type="float" step = "0.01" name="weight" class="form-control" required>
+							</div>
+							<div class="text-center">
+								<button type="submit" class="btn btn-sm btn-warning my-3 font-weight-bold">ยืนยัน</button>
+								<a class="btn btn-sm btn-danger my-3" href="index.php">ยกเลิก</a>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ testitf/insert.php
@@ -0,0 +1,48 @@
+<?php
+	$conn = mysqli_init();
+	mysqli_real_connect($conn, 'ihost.it.kmitl.ac.th', 'it63070215_215', '63070215', 'it63070215_215', 3306);
+	if(mysqli_connect_errno($conn)) {
+		die('Failed to connect to MySQL: '.mysqli_connect_error());
+	}
+	$name = $_POST['name'];
+	$height = $_POST['height'];
+    $weight = $_POST['weight'];
+    $bmi = $_POST['weight']/(($_POST['height']/100)**2)
+	$sql = "INSERT INTO itf (name, height, weight, bmi) VALUES ('$name', '$height', '$weight', '&bmi')";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ADD | 13th ITF LAB</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+	<style type="text/css">
+		.card {
+			border-top: solid 5px #ffc107;
+		}
+	</style>
+</head>
+<body class="bg-light py-5">
+	<div class="container text-dark">
+		<div class="row">
+			<div class="col-12 col-lg-8 offset-lg-2">
+				<div class="card shadow">
+					<div class="card-body">
+						<h2 align="center">
+						<?php
+							if(mysqli_query($conn, $sql)) {
+								echo "ADD COMPLETED";
+							}
+							else {
+								echo "FAILED TO ADD";
+							}
+						?>
+						</h2>
+						<p align="center" class="mt-4 mb-0"><a href="index.php" class="btn btn-link">BACK</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
